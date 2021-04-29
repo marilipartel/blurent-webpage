20f30cb0cc9c369ca63ee2133953a3ee572c4774
marilipartel <marili.partel@gmail.com>
2021-04-29 10:33:00 +0300

    FEATURE fixed carousel issues BLUR-9

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,14 +1,32 @@
 import "../styles/index.scss";
 import "../styles/carousel.scss";
 
-const productsCarousel = function () {
-  const carousel = document.querySelector(".carousel");
-  const slider = document.querySelector(".slider");
-  const leftArrow = document.querySelector(".left");
-  const rightArrow = document.querySelector(".right");
-  const noOfCards = document.querySelectorAll(".products__card").length;
-  let direction;
+//Products carousel elements
+const carouselP = document.querySelector(".carousel");
+const sliderP = document.querySelector(".slider");
+const leftArrowP = document.querySelector(".left");
+const rightArrowP = document.querySelector(".right");
+const noOfCardsP = document.querySelectorAll(".products__card").length;
+let directionP;
 
+//Trailer carousel elements
+const carouselT = document.querySelector(".carousel--t");
+const sliderT = document.querySelector(".slider--t");
+const leftArrowT = document.querySelector(".left--t");
+const rightArrowT = document.querySelector(".right--t");
+const noOfCardsT = document.querySelectorAll(".products__card--t").length;
+let directionT;
+
+//Infinite carousel function
+
+const infiniteCarousel = function (
+  carousel,
+  slider,
+  leftArrow,
+  rightArrow,
+  noOfCards,
+  direction
+) {
   slider.style.width = `${noOfCards * 436}px`;
 
   leftArrow.addEventListener("click", function () {
@@ -37,42 +55,22 @@ const productsCarousel = function () {
   });
 };
 
-productsCarousel();
+//Calling carousel function
 
-const trailerCarousel = function () {
-  const carouselT = document.querySelector(".carousel--t");
-  const sliderT = document.querySelector(".carousel--t");
-  const leftArrowT = document.querySelector(".left--t");
-  const rightArrowT = document.querySelector(".right--t");
-  const noOfCardsT = document.querySelectorAll(".products__card--t").length;
-  let directionT;
+infiniteCarousel(
+  carouselP,
+  sliderP,
+  leftArrowP,
+  rightArrowP,
+  noOfCardsP,
+  directionP
+);
 
-  sliderT.style.width = `${noOfCardsT * 436}px`;
-
-  leftArrowT.addEventListener("click", function () {
-    directionT = 1;
-    carouselT.style.justifyContent = "flex-end";
-    sliderT.style.transform = `translate(${100 / noOfCardsT}%)`;
-  });
-
-  sliderT.addEventListener("transitionend", function () {
-    if (directionT === -1) {
-      sliderT.appendChild(sliderT.firstElementChild);
-    } else if (directionT === 1) {
-      sliderT.prepend(sliderT.lastElementChild);
-    }
-    sliderT.style.transition = "none";
-    sliderT.style.transform = "translate(0)";
-    setTimeout(function () {
-      sliderT.style.transition = "all 0.3s";
-    });
-  });
-
-  rightArrowT.addEventListener("click", function () {
-    directionT = -1;
-    carouselT.style.justifyContent = "flex-start";
-    sliderT.style.transform = `translate(${-100 / noOfCardsT}%)`;
-  });
-};
-
-trailerCarousel();
+infiniteCarousel(
+  carouselT,
+  sliderT,
+  leftArrowT,
+  rightArrowT,
+  noOfCardsT,
+  directionT
+);
